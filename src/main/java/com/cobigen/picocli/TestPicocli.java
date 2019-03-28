a12221efe33c04a15882d84f0eaf0af4c11b98fa
sanjay <sanjay.h.yadav@capgemini.com>
2019-03-28 14:05:40 +0530

    After Refactoring Cobigen CLI code

--- src/main/java/com/cobigen/picocli/TestPicocli.java
+++ src/main/java/com/cobigen/picocli/TestPicocli.java
@@ -13,6 +13,7 @@ import org.eclipse.core.runtime.IPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cobigen.picocli.uitls.CreateJarFile;
 import com.devonfw.cobigen.api.CobiGen;
 import com.devonfw.cobigen.api.exception.InvalidConfigurationException;
 import com.devonfw.cobigen.api.extension.Merger;
@@ -41,20 +42,12 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 @Command(name = "TestPicocli", header = "%n@|TestPicocli Hello world demo|@")
-public class TestPicocli implements Runnable {
-	private static Logger logger =  LoggerFactory.getLogger(TestPicocli.class);
+public class TestPicocli  {
+	private static Logger logger = LoggerFactory.getLogger(TestPicocli.class);
 	GenerateMojo generateMojo = new GenerateMojo();
-	//ClassLoader cl = generateMojo.getProjectClassLoader();
-	/*@Parameters(index = "0", paramLabel = "Input", description = "Input to load.")
-	private File input;
-
-	@Parameters(index = "1", paramLabel = "LOG DIR", description = "Directory to store log files.")
-	String dir;*/
-    /**
-     * Location of workspace root
-     */
 	
-
+	
+	
 	public static boolean isFilenameValid(File input) {
 
 		try {
@@ -85,81 +78,38 @@ public class TestPicocli implements Runnable {
 		return true;
 	}
 
-	public void run() {
-	
-		//System.out.println("input, " + isFilenameValid(input));
-		//System.out.println("logDir, " + isValidDir(dir));
-	}
 	
 
 	public static void main(String... args) throws Exception {
-		// logger.info("start main method");
-		File jarFile = null;
-		File jarPath = new File("C:\\MyData\\IDE4\\workspaces\\.metadata\\cobigen_jar");
-		List<Object> inputs = Lists.newLinkedList();
-
+		logger.info("start main method");
 		logger.info("Please enter input from command prompt");
 		Scanner inputReader = new Scanner(System.in);
 		String userInput = inputReader.nextLine();
 		File inputFile = new File(userInput);
+		CreateJarFile createjarFile = new CreateJarFile();
+		File jarPath = new File("C:\\MyData\\IDE4\\workspaces\\.metadata\\cobigen_jar");
 
 		if (!jarPath.exists()) {
 			jarPath.mkdir();
 
 		}
+
 		try {
-
 			TemplatesJarUtil.downloadLatestDevon4jTemplates(true, jarPath);
-
-			logger.info("Successfully download source jar");
-
-			jarFile = TemplatesJarUtil.getJarFile(true, jarPath);
-			logger.info("get jar file");
-			if (jarFile != null) {
-				CobiGen cg = CobiGenFactory.create(jarFile.toURI());
-				JavaTriggerInterpreter javaTriger = new JavaTriggerInterpreter("java");
-				PluginRegistry.registerTriggerInterpreter(javaTriger);
-				TypeScriptMerger tsmerger = new TypeScriptMerger("tsmerge", false);
-				PluginRegistry.registerMerger(tsmerger);
-				XmlTriggerInterpreter xmlTriggerInterpreter = new XmlTriggerInterpreter("xml");
-				PluginRegistry.registerTriggerInterpreter(xmlTriggerInterpreter);
-
-				List<Merger> merger = Lists.newLinkedList();
-				merger.add(new TextAppender("textmergerActivator", false));
-
-				logger.info("After create method");
-
-				if (!inputFile.exists() || !inputFile.canRead()) {
-					logger.info("The file " + inputFile.getAbsolutePath() + " is not a valid input for CobiGen.");
-					throw new Exception("Could not read input file " + inputFile.getAbsolutePath());
-
-				}
-
-				Object input = InputPreProcessor.process(cg, inputFile, null);
-
-				List<IncrementTo> matchingIncrements = cg.getMatchingIncrements(input);
-				if (matchingIncrements.size() > 0) {
-
-					System.out.println("Increments Available = " + matchingIncrements.get(0).getDescription());
-				}
-				cg.generate(input, matchingIncrements, Paths.get("C:\\Users\\syadav9\\Desktop\\temp"));
-				System.out.println("Successfully generated Template");
-			}
+			createjarFile.validateFile(inputFile);
+			createjarFile.createJarAndGenerateIncr(inputFile);
 
 		} catch (MalformedURLException e1) {
-			// TODO Auto-generated catch block
+			// TODO explain here what kind of exception and why was it thrown
 			e1.printStackTrace();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		} catch (MojoFailureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		logger.info("successfully call cobigen create method");
 		System.out.println("successfully call cobigen create method");
 
-		// CommandLine.run(new TestPicocli(), System.err, args);
-
 	}
+
+	
 }
