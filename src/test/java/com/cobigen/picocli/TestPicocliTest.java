bb063c90a35df0337e8b643b1e94d2f26b7a2cb0
sanjay <sanjay.h.yadav@capgemini.com>
2019-03-28 19:55:38 +0530

    Provide reference path in Unit test case

--- src/test/java/com/cobigen/picocli/TestPicocliTest.java
+++ src/test/java/com/cobigen/picocli/TestPicocliTest.java
@@ -8,8 +8,11 @@ import java.util.Scanner;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestPicocliTest {
+	private static Logger logger = LoggerFactory.getLogger(TestPicocliTest.class);
 	/** Test resources root path */
     private static String testFileRootPath = "src/test/resources/testdata/unittest/";
 	@BeforeClass
@@ -25,10 +28,14 @@ public class TestPicocliTest {
 		try {
 			 File baseFile = new File(testFileRootPath + "EmployeeEntity.java");
 			 String args = "main";
+			 logger.info("Please enter input from command prompt");
+				Scanner in = new Scanner(System.in);
+				String s = in. nextLine();
+				assertEquals(baseFile.toString(), s);
 			TestPicocli.main(args);
-			Scanner in = new Scanner(System.in);
-			String s = in. nextLine();
-			assertEquals(baseFile, s);
+			
+			
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
